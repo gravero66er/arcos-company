fad0a3665221acf81016d70217822dccc51cafd9
gravero66er <49907144+gravero66er@users.noreply.github.com>
2022-01-09 08:52:01 +0500

    local fonts + dotenv.config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,12 @@
-const dotenv = require("dotenv")
+// const dotenv = require("dotenv")
 
-if (process.env.NODE_ENV !== "production") {
-  dotenv.config()
-}
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
+// if (process.env.NODE_ENV !== "production") {
+//   dotenv.config()
+// }
 
 module.exports = {
   /* Your site config here */
@@ -23,7 +27,7 @@ module.exports = {
     {
       resolve: `gatsby-source-contentful`,
       options: {
-        spaceId: `fci7friz7d54`,
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
         // Learn about environment variables: https://gatsby.dev/env-vars
         accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
       },
