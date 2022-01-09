65e14a63864dde89b40ee00d1988791c69c69565
gravero66er <49907144+gravero66er@users.noreply.github.com>
2022-01-09 11:32:54 +0500

    netlify update

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,6 +17,7 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-image`,
+    `gatsby-plugin-netlify`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
